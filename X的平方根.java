7cebf27b3e897f80485d83d5915d8ad4bd180e6a
niantiandi <95521077+niantiandi@users.noreply.github.com>
2022-09-16 18:54:52 +0800

    123

--- X的平方根.java
+++ X的平方根.java
@@ -2,27 +2,26 @@ import java.util.Scanner;
 
 public class X的平方根 {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        int x;
-        System.out.println("请输入X:");
-        x=input.nextInt();
-        System.out.println("结果为:"+MySqrt(x));
+        try (Scanner input = new Scanner(System.in)) {
+            int x;
+            System.out.println("请输入X:");
+            x=input.nextInt();
+            System.out.println("结果为:"+MySqrt(x));
+        }
     }
     public static int MySqrt(int x) {
-        int result=0;
-        int m,n;
-        if(x==1||x==0){
-            result=x;
+        if(x == 1)
+            return 1;
+        int min = 0;
+        int max = x;
+        while(max-min>1)
+        {
+            int m = (max+min)/2;
+            if(x/m<m)
+                max = m;
+            else
+                min = m;
         }
-        else{
-            m=x/2;
-            n=(m+x/m)/2;
-            while(m-n>1){
-                m=n;
-                n=(m+x/m)/2;
-            }
-            result=n;
-        }
-        return result;
+        return min;
     }
 }
