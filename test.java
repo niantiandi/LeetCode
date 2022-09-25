280147cdd31ce84668060294dea3a56ae60b26f0
niantiandi <95521077+niantiandi@users.noreply.github.com>
2022-09-25 10:13:29 +0800

    committed: 力扣秋季编程大赛第一题，气温变化趋势。

---
+++ test.java
@@ -0,0 +1,24 @@
+public class test {
+    public static void main(String[] args) {
+        int test[]={1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1};
+        int result[]=new int[test.length];
+        for (int i = 0; i < result.length; i++) {
+            result[i]=0;
+        }
+        int trans=0;
+        for (int i = 0; i < test.length; i++) {
+            if(test[i]==1){
+                result[trans]++;
+            }
+            else
+            trans++;
+        }
+        int max_result=0;
+        for (int i = 0; i < result.length; i++) {
+            if(result[i]>max_result){
+                max_result=result[i];
+            }
+        }
+        System.out.println(max_result);
+    }
+}
